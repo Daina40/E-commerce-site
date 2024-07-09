58d1e2f26f66971813c19a70ac80a634c08a5265
Daina40 <daina9438@gmail.com>
2024-07-09 18:31:29 +0600

    ecommerce

--- src/About.jsx
+++ src/About.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
 
 export default function About({newdata}) {
+    const imageList = [
+        {
+            image: "./image/MST. TASLIMA AKTER DAINA.jpg"
+        },
+        {
+            image: "./image/MST. TASLIMA AKTER DAINA.jpg"
+        },
+        {
+            image: "./image/MST. TASLIMA AKTER DAINA.jpg"
+        },
+        {
+            image: "./image/MST. TASLIMA AKTER DAINA.jpg"
+        }
+    ]
   return (
+    <>
+    {imageList.map((e)=>{
+        return (
+            <>
+            <img src={e.image} alt="" />
+            
+            </>
+        )
+    })}
     <div>
         <h1>This is Map section</h1>
     </div>
+    </>
+    
   )
-  
+
 }
